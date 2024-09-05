381d308f7db4943b89f9c0dcf1cbdc354158a0ae
Salman <fcsalmu234@gmail.com>
2024-09-05 21:50:04 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { Canvas} from '@react-three/fiber'
+import { useGLTF, Stage, PresentationControls } from '@react-three/drei'
+import {useState} from 'react'
+function Models(props){
+  const {scene}= useGLTF("/camelglb.glb");
+  return <primitive object={scene} {...props}/>
+
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Canvas dpr={[1,2]} shadows camera={{fov: 45}} style={{"position": "absolute"}}>
+      <color attach="background" args={["#101010"]}>
+      </color>
+      <PresentationControls speed={1.5} global zoom={.5} polar={[-0.1, Math.PI/4]}>
+        <Stage environment="">
+          <Models scale={0.01}/>
+        </Stage>
+      </PresentationControls>
+
+    </Canvas>
     </div>
   );
 }
